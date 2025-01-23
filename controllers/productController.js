de25cb65624ba2e9e0aa8fc2d986435589c7be7e
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-01-23 13:25:20 -0300

    created getAllProducts function

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,3 +8,16 @@ const getAllProducts = async (req, res) => {
         res.status(500).json({message: 'Products search error', error}) 
     }
 };
+
+const getProductById = async (req, res) => {
+    try {
+        const product = await Product.getProductById(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not find' });
+        }
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: 'Product search error', error})
+    }
+};
+
