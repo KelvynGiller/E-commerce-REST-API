57a5414fc2dbad2fe62da20e07d8b6c20ef56e0f
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-01-24 12:51:03 -0300

    updated user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const passport = require('passport');
+
 
 router.post('/register', userController.registerUser);
+
+router.get('/', passport.authenticate('jwt', { session: false }), userController.getAllUsers);
+
+router.get('/:userId', passport.authenticate('jwt', { session: false }), userController.getUserById);
+
+router.put('/:userId', passport.authenticate('jwt', { session: false }), userController.updateUser);
+
+router.delete('/:userId', passport.authenticate('jwt', { session: false }), userController.deleteUser);
 
 module.exports = router;
