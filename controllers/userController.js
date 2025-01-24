8d45b97ad2afc854636bbc33a649bd86d1a06138
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-01-24 12:34:58 -0300

    created getUserById function

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,10 +35,26 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+const getUserById = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const result = await pool.query('SELECT id, username, email, created_at FROM users WHERE id = $1', [userId]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching user', error });
+    }
+};
+
 
 
 module.exports = {
     registerUser,
     getAllUsers,
-    
+
 };
